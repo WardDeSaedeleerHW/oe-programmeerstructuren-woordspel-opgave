5da94bb2a6742a5601e6ddf9cd9d60af9f503b17
WardDeSaedeleerHW <ward.de.saedeleer@student.howest.be>
2020-03-03 14:07:40 +0100

    Oplossing

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener('load',Initieer);
 
 function Initieer(){
   KoppelDomElementen();
-  StandaardInput();
+  KoppelEvents();
 }
 
 function KoppelDomElementen() {
@@ -23,22 +23,54 @@ function KoppelDomElementen() {
 }
 
 function CheckPalindroom() {
- 
+  let woord = txtWoord.value;
+
+  let splitString = woord.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+
+  if (woord == joinArray) {
+    divResultaat.innerHTML = woord + " is een palindroom.";
+   } else {
+    divResultaat.innerHTML = woord + " is geen palindroom";
+   }   
 }
 
 function GeefPlaatsEersteA() {
+  let woord = txtWoord.value;
+  let woordLength = woord.length;
+  let plaats = 0, gevonden = false;
 
+  while(plaats < woordLength && !gevonden){
+    if (woord.substr(plaats, 1) == "a"){
+      gevonden = true;
+    }
+    plaats++;
+  }
+
+  divResultaat.innerHTML = gevonden ? "De eerste 'a' in het woord " + woord + " staat op positie " + plaats : "Er zit geen letter a in het woord " + woord;
 }
 
 function KeerOm() {
+  let woord = txtWoord.value;
 
-}
+  let splitString = woord.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
 
-function StandaardInput() {
-  txtWoord.value = "radarke";
+  divResultaat.innerHTML = joinArray;
 }
 
 function ToonVertikaal(){
+  let woordVertikaal = "";
+  let woord = txtWoord.value;
+  let aantalLetters = woord.length;
 
+  for (let i = 0; i < aantalLetters; i++){
+    let teken = woord.charAt(i);
+    woordVertikaal += teken + '<br>';
+  };
+
+  divResultaat.innerHTML = woordVertikaal;
 }
 
